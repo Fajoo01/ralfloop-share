32d0bc9ed2b1f102015f4750cdf955fb93c7417c
Fabio Fagioli <fajoo01@gmail.com>
2026-09-18 10:35:57 +0200

    Bind Mailchimp preview text to send scope

--- tests/test_mailchimp_campaign_runtime.py
+++ tests/test_mailchimp_campaign_runtime.py
@@ -24,7 +24,8 @@ class FakeProvider:
         self.campaigns[campaign_id] = {
             "campaign_id": campaign_id, "list_id": scope["list_id"],
             "subject": scope["subject"], "from_name": scope["from_name"],
-            "reply_to": scope["reply_to"], "content_sha256": scope["body_sha256"],
+            "reply_to": scope["reply_to"], "preheader": scope["preheader"],
+            "title": scope["internal_title"], "content_sha256": scope["body_sha256"],
             "html_sha256": scope["html_sha256"],
             "sent": False,
         }
