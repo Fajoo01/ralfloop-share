e0edd351fa65dd4bbc80b1567780bd9322cff025
Fabio Fagioli <fajoo01@gmail.com>
2026-09-26 23:52:22 +0200

    gpt browser: always focus dedicated blue profile

--- ralfloop_agent/integration/gpt_frontend.py
+++ ralfloop_agent/integration/gpt_frontend.py
@@ -622,6 +622,8 @@ class GptWorkController:
         else:
             target_id = self.cdp.create_target(spec["url"], background=False)
             created = True
+        if hasattr(self.cdp, "raise_browser_window"):
+            self.cdp.raise_browser_window()
         return {
             "action": "provider_opened",
             "provider": name,
